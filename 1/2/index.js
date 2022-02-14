ae571ce20c417ff7295b8900c4b6ece256ad8d92
frisbee9 <zabolotnyy.d@gmail.com>
2022-02-14 19:21:27 +0200

    21

--- 1/2/index.js
+++
@@ -1,8 +0,0 @@
-   $( document ).ready(function() {
-        $('#s-h-pass').click(function(){
-            var type = $('#password').attr('type') == "text" ? "password" : 'text',
-             c = $(this).text() == "Скрыть пароль" ? "Показать пароль" : "Скрыть пароль";
-            $(this).text(c);
-            $('#password').prop('type', type);
-        });
-    });
